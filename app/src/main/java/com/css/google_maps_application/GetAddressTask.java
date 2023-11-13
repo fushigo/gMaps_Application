fea1e54f09fd53cfeb52831762ca22fc45891aa4
Fushigo <rizky23ska@gmail.com>
2023-11-13 14:19:49 +0700

    update get address

--- app/src/main/java/com/css/google_maps_application/GetAddressTask.java
+++ app/src/main/java/com/css/google_maps_application/GetAddressTask.java
@@ -1,8 +1,14 @@
 package com.css.google_maps_application;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class GetAddressTask extends AsyncTask<Location, Void, String> {
     private Context mContext;
@@ -15,7 +21,18 @@ public class GetAddressTask extends AsyncTask<Location, Void, String> {
 
     @Override
     protected String doInBackground(Location... locations) {
-        return null;
+        Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
+        Location location = locations[0];
+        List<Address> addresses = null;
+        String result = "";
+
+        try{
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        } catch (IOException e) {
+            result = "Service Not Available";
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     interface OnTaskCompleted{
